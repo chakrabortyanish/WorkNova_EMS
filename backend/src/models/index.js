c9b3d5c52df10b55f40cf7f06c89bcdd7f4ee2b6
Anish Chakraborty <chakrabortyanish981@gmail.com>
2026-08-10 20:15:31 +0530

    importend all files in a single file

---
+++ backend/src/models/index.js
@@ -0,0 +1,7 @@
+import Admin from "./admin.model";
+import Employee from "./employee.model";
+import Attendance from "./attendance.model";
+import Leave from "./leave.model";
+import Payslip from "./payslip.model";
+
+export { Admin, Employee, Attendance, Leave, Payslip };
